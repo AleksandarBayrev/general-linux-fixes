dbe48683ad85b86b586ba1caeb1f5d1971460f08
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-01-31 17:02:22 +0200

    Update script

--- set-cpu-freq.sh
+++ set-cpu-freq.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo 2501000 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
-echo 3501000 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
-echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo 1 | sudo tee /sys/devices/system/cpu/cpufreq/boost
+sudo cpupower frequency-set -u 3.5GHz -g powersave
