f99804dd4bd656bba61151e69ebb729628f68d05
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-05-03 21:24:09 +0300

    Update set-cpu-freq

--- set-cpu-freq.sh
+++ set-cpu-freq.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo cpupower frequency-set -d 2.5GHz -u 2.5GHz -g performance
+
+# If using amd_pstate=passive use this script (comment out the line above and uncomment this)
+# sudo cpupower frequency-set -g performance
+# echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost 
