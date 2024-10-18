20dbee9eee904b0adefd1e15d51bd195315e1410
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-10-18 20:00:33 +0300

    Update script

--- set-cpu-freq.sh
+++ set-cpu-freq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cpupower frequency-set -d 2.5GHz -u 2.5GHz -g performance
+sudo cpupower frequency-set -d 2.5GHz -u 2.5GHz -g powersave # or performance
 
 # If using amd_pstate=passive use this script (comment out the line above and uncomment this)
 # If you use a speed higher than base clock (in range of turbo boost, let's say base clock is 2.5GHz and turbo boost is up to 5.2GHz, but you want 4.0GHz - do not uncomment the line below)
